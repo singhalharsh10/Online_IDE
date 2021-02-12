947345b2f9af75db3faa1cef6ca40065559dddff
singhalharsh10 <hsinghal1_be18@thapar.edu>
2021-02-12 15:35:05 +0530

    final commit

---
+++ Backend/main.js
@@ -0,0 +1,81 @@
+var hackerearth = require('hackerearth-node');
+var hackerearth = new hackerearth('203157672c819e6d5ca8ace68ad81fbe08758ba6');
+
+var config = {};
+config.time_limit = 2;
+config.memory_limit = 323244;
+// config.input = "";
+config.language = "C++";
+
+async function compile(code, input, language) {
+    try {
+        config.source = code;
+        config.input = input;
+        config.language = language;
+        let result = await JSON.parse(await hackerearth.compile(config));
+        if (result.compile_status == "OK") {
+            return { "status": 2, "final": result };
+        } else {
+            return { "status": 1, "final": result };
+        }
+    } catch (err) {
+        return { "status": 0, "final": result };
+    }
+}
+
+async function run(code, input, language) {
+    try {
+        config.source = code;
+        config.input = input;
+        config.language = language;
+        let result = await JSON.parse(await hackerearth.run(config));
+        if (result.run_status.status == "AC") {
+            return { "status": 2, "final": result };
+        } else {
+
+            console.log("bahar " + result);
+            return { "status": 1, "final": result };
+        }
+
+    } catch (err) {
+        console.log("JAI Hind");
+        return { "status": 0, "final": result };
+    }
+}
+exports = module.exports = {
+    compile,
+    run
+}
+
+// function compile(code, req, res, callback) {
+//     config.source = code;
+//     hackerearth.compile(config, function (err, result) {
+//         if (err) {
+//             console.log("-----------------Error in Compiling---------------------")
+//             console.log(err);
+//             res.send(err);
+//             callback({ "status": 0 });
+//         } else {
+//             console.log("Compiled Succesfully " + JSON.parse(result));
+//             res.send(JSON.parse(result));
+//             callback({ "status": 1 });
+//         }
+//     });
+// }
+//
+// function run(code, req, res, callback) {
+//     config.source = code;
+//     hackerearth.run(config, function (err, result) {
+//         if (err) {
+//             console.log("-----------------Error in running----------------------")
+//             console.log(err);
+//             res.send(err);
+//             callback({ "status": 0 });
+//         } else {
+//             console.log("--------------------Run ho gaya hai bhaya--------------");
+//             console.log(result);
+//             res.send(result);
+//             callback({ "status": 1 });
+//         }
+//     });
+// }
---
+++ Frontend/index.js
@@ -0,0 +1,190 @@
+let socket = io();
+socket.on('connected', () => {
+    console.log("Connected " + socket.id)
+})
+
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/twilight");
+editor.session.setMode("ace/mode/c_cpp");
+let inputbox = $('#input');
+let outputbox = $('#output');
+let time = $('#time');
+let memory = $('#memory');
+let share = $('#save');
+let writelang = $('#write_lang');
+let my_editor = $('#editor');
+
+let wait = "Please wait, Your code is in process phase .......";
+
+let language1 = "C++";
+
+let custom_position = {
+    row: 0,
+    column: 0
+};
+
+let java_text = `/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+    public static void main (String[] args) {
+        System.out.println("Harsh!");
+    }
+}`
+
+let cplusplus_text = `#include<bits/stdc++.h>;
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define nt _int128
+#define inf (int)1e9
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    // cin >> t;
+    t = 1;
+    while(t--)
+    {
+
+    }
+    return 0;
+}`
+
+let python_text = `#code
+print("Harsh")`
+
+editor.session.insert(custom_position, cplusplus_text);
+
+// -----------------LANGUAGES--------------------------------
+$("#java").click(function() {
+    editor.session.setMode("ace/mode/java");
+    language1 = "JAVA";
+    writelang.empty();
+    writelang.append('<h6>  JAVA </h6>');
+    editor.setValue("");
+    editor.session.insert(custom_position, java_text);
+});
+
+$("#c").click(function() {
+    editor.session.setMode("ace/mode/c_cpp");
+    language1 = "C++"
+    writelang.empty();
+    writelang.append('<h6>  C++ </h6>');
+    editor.setValue("");
+    editor.session.insert(custom_position, cplusplus_text);
+});
+
+$("#python").click(function() {
+    editor.session.setMode("ace/mode/python");
+    language1 = "PYTHON";
+    writelang.empty();
+    writelang.append('<h6> Python </h6>');
+    editor.setValue("");
+    editor.session.insert(custom_position, python_text);
+});
+
+$("#fullscreen").click(function() {
+    editor.container.webkitRequestFullscreen()
+})
+
+//-------------------THEMES----------------------------------------
+$("#twi").click(function() {
+    editor.setTheme("ace/theme/twilight");
+});
+
+$("#amb").click(function() {
+    editor.setTheme("ace/theme/ambiance");
+});
+
+$("#cha").click(function() {
+    editor.setTheme("ace/theme/chaos");
+});
+
+$("#rome").click(function() {
+    editor.setTheme("ace/theme/chrome");
+});
+
+$("#mid").click(function() {
+    editor.setTheme("ace/theme/clouds_midnight");
+});
+
+$("#clo").click(function() {
+    editor.setTheme("ace/theme/clouds");
+});
+
+$("#cri").click(function() {
+    editor.setTheme("ace/theme/crimson_editor");
+});
+
+$("#ecl").click(function() {
+    editor.setTheme("ace/theme/eclipse");
+});
+
+$("#mer").click(function() {
+    editor.setTheme("ace/theme/merbivore");
+});
+
+$("#nord").click(function() {
+    editor.setTheme("ace/theme/nord_dark");
+});
+
+$("#sol").click(function() {
+    editor.setTheme("ace/theme/solarized_dark");
+});
+
+
+//--------------------- MAIN LOGIC ---------------------------------
+$("#first_button").click(function() {
+    let code = editor.getValue();
+    let input1 = inputbox.val();
+    outputbox.empty();
+    time.empty();
+    memory.empty();
+    share.empty();
+    outputbox.append(wait);
+    // console.log(code);
+    socket.emit('send', {
+        task: code,
+        input: input1,
+        language: language1
+    })
+
+    socket.on('rcv', function(data) {
+        // console.log("index.js " + data.code_id + " " + socket.id + " " + "Hello" + data.run_status.output);
+        if (data.compile_status === "OK") {
+            outputbox.empty();
+            time.empty();
+            outputbox.append(data.run_status.output);
+            time.replaceWith('<h6>' + data.run_status.time_used + '<h6>');
+            memory.replaceWith('<h6>' + data.run_status.memory_used + '<h6>');
+            share.replaceWith('<h6>' + data.code_id + '<h6>');
+        } else {
+            outputbox.empty();
+            outputbox.append(data.compile_status);
+        }
+    })
+
+    // console.log(input);
+    // $.post('/todo/', { task: code, input: input1, language: language1 }, function (data) {
+    //     console.log("--------------------------");
+    //     console.log(data);
+    //     // console.log(data.run_status.output);
+    //     if (data.compile_status === "OK") {
+    //         outputbox.empty();
+    //         outputbox.append(data.run_status.output);
+    //         time.append('<h6>' + data.run_status.time_used + '<h6>');
+    //         memory.append('<h6>' + data.run_status.memory_used + '<h6>');
+    //         share.append('<h6>' + data.web_link + '<h6>');
+    //     }
+    //     else {
+    //         outputbox.empty();
+    //         outputbox.append(data.compile_status);
+    //     }
+    // })
+})
